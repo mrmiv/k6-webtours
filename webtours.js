ab622cfe5a94ea5b3b9d4da0de959c3898fd3277
Markov Ilya <markoviv@aldera.loc>
2023-07-24 13:34:54 +0700

    add login and flight scenarios

--- webtours.js
+++ webtours.js
@@ -1,61 +1,115 @@
 import http from 'k6/http'
 import { check, group } from 'k6'
-import { SharedArray } from 'k6/data';
+import { randomItem } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 
-const BASE_URL = 'https://test.k6.io'
-
-const data = new SharedArray('users', () => {
-  return JSON.parse(open('./users.json')).user
-})
+const BASE_URL = 'http://webtours.load-test.ru:1080/cgi-bin'
 
 export const options = {
 //   discardResponseBodies: true, // if you check only http-code
-  scenarios: {
-    base: {
-      executor: 'constant-vus',
-      exec: 'getBase',
-      vus: 50,
-      duration: '30s',
-    },
-    features: {
-      executor: 'per-vu-iterations',
-      exec: 'getFeatures',
-      vus: 10,
-      iterations: 100,
-      startTime: '10s',
-      maxDuration: '1m',
-    },
-  },
-  thresholds: {
-    http_req_failed: ['rate<1'],
-    http_req_duration: ['p(95)<300'],
-  },
+//   scenarios: {
+//     base: {
+//       executor: 'constant-vus',
+//       exec: 'getBase',
+//       vus: 50,
+//       duration: '30s',
+//     },
+//     features: {
+//       executor: 'per-vu-iterations',
+//       exec: 'getFeatures',
+//       vus: 10,
+//       iterations: 100,
+//       startTime: '10s',
+//       maxDuration: '1m',
+//     },
+//   },
+//   thresholds: {
+//     http_req_failed: ['rate<1'],
+//     http_req_duration: ['p(95)<300'],
+//   },
 };
 
-export function getBase() {
-  // const res = http.get(BASE_URL);
-  // check(res, {
-  //   'status code is 200': (res) => res.status === 200,
-  // });
+// LOAD SCENARIO
+export default function () {
+    
+  let flightParams = {
 
-  // const payload = { name: 'Bert' };
-  // const headers = { headers: { 'Content-Type': 'application/json' } };
-  // const resPost = http.post(
-  //   'https://test.k6.io/flip_coin.php',
-  //   JSON.stringify(payload),
-  //   headers,
-  // );
-  // check(resPost, {
-  //   'status code is 200': (resPost) => resPost.status === 200,
-  // });
+    'departCity': '',
+    'arriveCity': '',
+
+    'departDate': '', // ISO formant 2023-07-20 - Date.now() 
+    'returnDate': '', // ISO formant 2023-07-20 - Date.now() 
+
+    'seatPref': '',
+    'seatType': '',
+
+    'advanceDiscount': '0',
+    'numPassengers': '1',
+
+    'roundtrip': undefined, // on or undefined
+
+    'outboundFlight': '',
+
+  }
+
+  let user = {
+    "username": "marco", 
+    "password": "test123",
+    'userSession': '',
+    
+    'lastName': '',
+    'firsName': '',
+
+    'address1': '', // city
+    'address2': '', // adress
+
+    'pass1': '',
+    'creditCard': '',
+    'expDate': ''
+  }
+
+   group("home", () => {
+
+    getWelcomePage()
+    user['userSession'] = getNavAtHome()
+    getHomePage()
+
+  })
+
+  group("login", () => {
+
+    userLogin(user.userSession, user.username, user.password)
+    getLoggedInNav()
+
+  })
+
+  group("flights", () => {
+
+    getSearchFlightPage()
+    getNavigationInFlights()
+    const {departCity, arriveCity, seatPref, seatType} = getReservationsList()
+
+    flightParams['departCity'] = departCity
+    flightParams['arriveCity'] = arriveCity
+    flightParams['seatPref'] = seatPref
+    flightParams['seatType'] = seatType
+    flightParams['departDate'] = new Date().toISOString()
+    flightParams['returnDate'] = new Date().toISOString()
+
+
+
+  })
+
 }
 
-
-// HOME
+///// HOME /////
 
 export function getWelcomePage(){
+
+  const params = {
+    'signOff': true
+  }
   
-  const res = http.get(`${BASE_URL}/welcome.pl`, {'signOff': true})
+  const res = http.get(`${BASE_URL}/welcome.pl`, params)
   check(res, {
     'get welcome page status is 200': res => res.status === 200
   })
@@ -63,14 +117,17 @@ export function getWelcomePage(){
 }
 
 export function getNavAtHome(){
+
+  const params = {
+    'in': 'home'
+  }
   
-  const res = http.get(`${BASE_URL}/nav.pl`, {'in': 'home'})
+  const res = http.get(`${BASE_URL}/nav.pl`, params)
   check(res, {
     'get navigation in home status is 200': res => res.status === 200
   })
 
-  const userSession = res.html().find('input[name="userSession"]').first().attr("value")
-
+  const userSession = res.html().find('input[name="userSession"]').val()
   return userSession
 
 }
@@ -84,32 +141,117 @@ export function getHomePage(){
 
 }
 
-// LOGIN
-export function userLogin(userSession){
+///// LOGIN /////
 
-}
+export function userLogin(userSession, username, password){
 
-// FLIGHT
-export function getFlightOptions(){
-
-}
-
-
-// LAOD SCENARIO
-export default function () {
+  const payload = {
+    'userSession': userSession,
+    'username': username,
+    'password': password
+  }
   
-  const userSession = group("home", () => {
-    getWelcomePage()
-    const userSession = getNavAtHome()
-    getHomePage()
+  const headers = {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded' 
+    }
+  }
 
-    return userSession
+  const res = http.post(`${BASE_URL}/login.pl`, payload, headers)
+  check(res, {
+    'login status is 200': res => res.status === 200
   })
 
-  group("login", () => userLogin(userSession))
+}
 
-  group("flights", () => {
-    getFlightOptions()
+export function getLoggedInNav(){
+
+  const params = { 
+    'in': 'home',
+    'page': 'menu'
+  }
+
+  const res = http.get(`${BASE_URL}/nav.pl`, params)
+  check(res, {
+    'get authorized nav in home status is 200': res => res.status === 200
+  })
+
+}
+
+///// FLIGHT /////
+
+export function getSearchFlightPage(){
+
+  const params = {
+    'page': 'search'
+  }
+
+  const res = http.get(`${BASE_URL}/welcome.pl`, params)
+  check(res, {
+    'get search flight page status is 200': res => res.status === 200
+  })
+
+}
+
+export function getNavigationInFlights() {
+  
+  const params = {
+    'page': 'menu',
+    'in': 'flights'
+  }
+
+  const res = http.get(`${BASE_URL}/nav.pl`, params)
+  check(res, {
+    'get nav in flights page status is 200': res => res.status === 200
+  })
+
+}
+
+export function getReservationsList(){
+
+  const params = {
+    'page': 'welcome'
+  }
+
+  const res = http.get(`${BASE_URL}/reservations.pl`, params)
+  check(res, {
+    'get reservations list status is 200': res => res.status === 200
+  })
+
+  let flightParams = {
+    'departCity': '',
+    'arriveCity': '',
+    'seatPref': '',
+    'seatType': '',
+  }
+
+  flightParams['departCity'] = randomItem(res.html().find('select[name=depart]').children().map((idx, el) => el.val()))
+  flightParams['arriveCity'] = randomItem(res.html().find('select[name=arrive]').children().map((idx, el) => el.val()))
+  flightParams['seatPref'] = randomItem(res.html().find('input[name=seatPref]').map((idx, el) => el.val()))
+  flightParams['seatType'] = randomItem(res.html().find('input[name=seatType]').map((idx, el) => el.val()))
+
+  return flightParams
+
+}
+
+export function setFlightOptions(){
+
+  const payload = {
+    'findFlights.y': '9',
+    'findFlights.x': '54',
+    '.cgifields': ['roundtrip', 'seatType', 'seatPref']
+  }
+
+  const headers = {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Upgrade-Insecure-Requests': '1'
+    }
+  }
+
+  const res = http.post(`${BASE_URL}/reservations.pl`, payload, headers)
+  check(res, {
+    'set flight options status is 200': res => res.status === 200
   })
 
 }
